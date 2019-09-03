71b4329c73761a2727408b1e450f58d3844ce00f
James Grantham <james.r.grantham@gmail.com>
2019-09-03 14:18:32 +0100

    Add keydown event listener

--- js/index.js
+++ js/index.js
@@ -4,3 +4,22 @@ document.querySelector('.intro img').addEventListener('mouseover', () => {
     event.target.style.width = '400px'
 });
 
+let heading = document.querySelector('h1');
+document.addEventListener('keydown', () => {
+    heading.style.backgroundColor = 'red'
+})
+
+let boat = document.querySelector('.content-destination img');
+
+
+
+
+
+
+
+
+// let subHeading = document.querySelectorAll('h2');
+// console.log(subHeading);
+// subHeading.addEventListener('keydown', () => {
+//     subHeading.forEach(el => el.style.color = 'red')
+// });
